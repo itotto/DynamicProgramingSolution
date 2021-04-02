0cd28c3b68c7c696dcb50bc5ebb609b9b9c02de6
itotto0205 <itotto0205@gmail.com>
2021-04-03 01:04:51 +0900

    階段の上り方新規追加

---
+++ stairsup001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stairsup001 {
+    class Program {
+        static void Main(string[] args) {
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            // 返却値を初期化
+            var r = new List<int>(input) { 1 };
+            for (var i = 1; i <= input; i++) {
+                r.Add(0);
+                // 1段上る
+                if (i >= 1) r[i] += r[i - 1];
+
+                // 2段上る
+                if (i >= 2) r[i] += r[i - 2];
+            }
+
+            Console.WriteLine(r[input]);
+        }
+    }
+}
---
+++ stairsup003/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stairsup003 {
+    class Program {
+        static void Main() {
+
+            var inputs = Console.ReadLine().Split(' ');
+            var n = Convert.ToInt32(inputs[0]);
+
+            var upunit = new List<int>();
+            for (var i = 1; i < inputs.Length; i++) {
+                upunit.Add(Convert.ToInt32(inputs[i]));
+            }
+            upunit.Sort((x, y) => x - y);
+
+            // 結果
+            var r = new List<int> { 1 };
+            for (var i = 1; i <= n; i++) {
+                r.Add(0);
+                foreach (var u in upunit) {
+                    if (i >= u) r[i] += r[i - u];
+                }
+            }
+
+            // 結果表示
+            Console.WriteLine(r[r.Count - 1]);
+        }
+    }
+}
