c2a8b93ebf091533c20ad5923af998578c3f645c
itotto0205 <itotto0205@gmail.com>
2021-05-31 00:53:06 +0900

    修正完了（改善する余地があるのであとで直す）

--- subsetsum003/Program.cs
+++ subsetsum003/Program.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 
 namespace subsetsum003 {
+    /// <summary>
+    /// 部分和問題 3
+    /// </summary>
+    /// <remarks>https://paiza.jp/works/mondai/dp_primer/dp_primer_partial_sums_step2/edit?language_uid=c-sharp</remarks>
     class Program {
 
         const int NOT_FOUND = -1;
@@ -11,27 +15,22 @@ namespace subsetsum003 {
         /// </summary>
         static List<int> _weights = null;
 
-        /// <summary>
-        /// 求めたい値(和)
-        /// </summary>
-        static int _targetValue = -1;
-
-        static void Main(string[] args) {
+        static void Main() {
             // 件数と目標値を入力
             var conditions = Console.ReadLine().Split(' ');
             var n = Convert.ToInt32(conditions[0]);
-            _targetValue = Convert.ToInt32(conditions[1]);
+            var targetValue = Convert.ToInt32(conditions[1]);
 
             var depth = NOT_FOUND;
 
             _weights = new List<int>(n);
             for (var i = 0; i < n; i++) {
                 var w = Convert.ToInt32(Console.ReadLine());
-                if (w == _targetValue) {
+                if (w == targetValue) {
                     depth = 1;
                     break;
                 }
-                if (w < _targetValue) _weights.Add(w); // xより重いものは排除
+                if (w < targetValue) _weights.Add(w); // xより重いものは排除
             }
 
             // 見つかってなかった場合のみ
@@ -42,7 +41,7 @@ namespace subsetsum003 {
                 _weights.Sort((x, y) => x - y);
 
                 for (var i = n - 1; i >= 0; i--) {
-                    var f = FindTargetCombination(_weights[i], i - 1);
+                    var f = FindTargetCombination(targetValue, _weights[i], i - 1);
                     if (f != NOT_FOUND) {
                         if (depth == NOT_FOUND || f < depth) depth = f + 1;
                     }
@@ -55,16 +54,23 @@ namespace subsetsum003 {
         }
 
         /// <summary>
-        /// 計算結果を保存しておく
+        /// 計算結果を保存しておく(キャッシュ)
         /// </summary>
         static Dictionary<string, int> _calculated = new Dictionary<string, int>();
 
-        static int FindTargetCombination(int currentValue, int startPos) {
+        /// <summary>
+        /// 組み合わせを見つける
+        /// </summary>
+        /// <param name="targetValue">最終的に欲しい値</param>
+        /// <param name="currentValue">いまの値</param>
+        /// <param name="startPos">確認する場所</param>
+        /// <returns></returns>
+        static int FindTargetCombination(int targetValue, int currentValue, int startPos) {
             var key = $"{currentValue}-{startPos}";
             if (_calculated.ContainsKey(key)) return _calculated[key];
 
             var depth = NOT_FOUND;
-            var need = _targetValue - currentValue;
+            var need = targetValue - currentValue;
             for (var i = startPos; i >= 0; i--) {
                 var w = _weights[i];
                 if (w > need) {
@@ -74,9 +80,9 @@ namespace subsetsum003 {
                     break;
                 } else if (w < need) {
                     if (i > 0) {
-                        var d = FindTargetCombination(currentValue + w, i - 1);
+                        var d = FindTargetCombination(targetValue, currentValue + w, i - 1);
                         if (d != NOT_FOUND) {
-                            depth = d + 1;
+                            if (depth == NOT_FOUND || d < depth) depth = d + 1;
                         }
                     }
                 }
